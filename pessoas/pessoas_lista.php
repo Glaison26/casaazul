38cf9a61413a30b5d3d8f7a0760121540c10e58f
Glaison <Glaison26.queiroz@gmail.com>
2026-03-25 16:19:29 -0300

    dev

--- pessoas/pessoas_lista.php
+++ pessoas/pessoas_lista.php
@@ -12,13 +12,69 @@ $c_sql = "SELECT * FROM cadastro ORDER BY nome";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Pessoas Físicas</title>
+    <script>
+        $(document).ready(function() {
+            $('.tabpessoas').DataTable({
+                // 
+                "iDisplayLength": -1,
+                "order": [1, 'asc'],
+                "aoColumnDefs": [{
+                    'bSortable': false,
+                    'aTargets': [7]
+                }, {
+                    'aTargets': [0],
+                    "visible": true
+                }],
+                "oLanguage": {
+                    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                    "sLengthMenu": "_MENU_ resultados por página",
+                    "sInfoFiltered": " - filtrado de _MAX_ registros",
+                    "oPaginate": {
+                        "spagingType": "full_number",
+                        "sNext": "Próximo",
+                        "sPrevious": "Anterior",
+                        "sFirst": "Primeiro",
+                        "sLoadingRecords": "Carregando...",
+                        "sProcessing": "Processando...",
+                        "sZeroRecords": "Nenhum registro encontrado",
+
+                        "sLast": "Último"
+                    },
+                    "sSearch": "Pesquisar",
+                    "sLengthMenu": 'Mostrar <select>' +
+                        '<option value="5">5</option>' +
+                        '<option value="10">10</option>' +
+                        '<option value="20">20</option>' +
+                        '<option value="30">30</option>' +
+                        '<option value="40">40</option>' +
+                        '<option value="50">50</option>' +
+                        '<option value="-1">Todos</option>' +
+                        '</select> Registros'
+
+                }
+
+            });
+
+        });
+    </script>
+
+</head>
 
 <body>
-    <div class="container>
-        
+    <div class="container-fluid">
+        <div class="panel panel-primary class">
+            <div class="panel-heading text-center">
+                <h4>Casa Azul - Sistema de Gestão</h4>
+                <h5>Lista de Pessoas Fisicas<h5>
+            </div>
+        </div>
+        <br>
+        <a class="btn btn-success btn-sm" href="/gop/cadastros/materiais/materiais_novo.php"><span class="glyphicon glyphicon-plus"></span> Incluir</a>
+        <a class="btn btn-secondary btn-sm" href="/gop/menu.php"><span class="glyphicon glyphicon-off"></span> Voltar</a>
+        <hr>
         <div class="table-responsive">
-            <table class="table table-striped table-hover">
-                <thead class="table-dark">
+            <table class="table table-bordered table-striped tabpessoas">
+                <thead class="thead">
                     <tr>
                         <th>Nome</th>
                         <th>Endereço</th>
@@ -27,6 +83,7 @@ $c_sql = "SELECT * FROM cadastro ORDER BY nome";
                         <th>Telefone 1</th>
                         <th>Telefone 2</th>
                         <th>Sexo</th>
+                        <th>Opções</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,13 +98,13 @@ $c_sql = "SELECT * FROM cadastro ORDER BY nome";
                     while ($c_linha = $result->fetch_assoc()) {
                         echo "
                     <tr>
-                        <td>></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>$c_linha[nome]</td>
+                        <td>$c_linha[endereco]</td>
+                        <td>$c_linha[bairro]</td>
+                        <td>$c_linha[cep]</td>
+                        <td>$c_linha[fone1]</td>
+                        <td>$c_linha[fone2]</td>
+                        <td>$c_linha[fone2]</td>
                         <td>
                     <a class='btn btn-secondary btn-sm' href='/gop/cadastros/fabricantes/fabricantes_editar.php?id=$c_linha[id]'><span class='glyphicon glyphicon-pencil'></span> Editar</a>
                     <a class='btn btn-danger btn-sm' href='javascript:func()'onclick='confirmacao($c_linha[id])'><span class='glyphicon glyphicon-trash'></span> Excluir</a>
