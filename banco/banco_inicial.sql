984cbba961cab2335edf993b6dcfdaba6d0b430a
Glaison <Glaison26.queiroz@gmail.com>
2026-04-01 16:39:04 -0300

    dev

--- banco/banco_inicial.sql
+++ banco/banco_inicial.sql
@@ -44,9 +44,50 @@ CREATE TABLE IF NOT EXISTS `cadastro` (
   `datasocio` date DEFAULT NULL,
   `observacao` blob,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela casaazul.cadastro: ~1 rows (aproximadamente)
+INSERT INTO `cadastro` (`id`, `nome`, `datanasc`, `identidade`, `cpf`, `cep`, `endereco`, `bairro`, `cidade`, `nomepai`, `nomemae`, `fone1`, `fone2`, `fone3`, `niss`, `email`, `data_cadastro`, `numerofilhos`, `sexo`, `socio`, `datasocio`, `observacao`) VALUES
+	(4, 'Glaison Queiroz', '1968-10-26', '4662097', '69551022653', '34505480', 'Rua da Intendencia 316', 'Centro', 'Sabará', 'Valdir Queiroz', 'Emilia Pereira Queiroz', '(31) 98426-2508', '(31) 3672-7688', '', '45465465', 'glaison26.queiroz@gmail.com', '2026-04-01', 0, 'M', NULL, NULL, _binary '');
+
+-- Copiando estrutura para tabela casaazul.cursos
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `DESCRICAO` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `OBSERVACAO` blob,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela casaazul.cadastro: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela casaazul.cursos: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela casaazul.instrutores
+CREATE TABLE IF NOT EXISTS `instrutores` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(150) NOT NULL,
+  `CEP` char(12) NOT NULL,
+  `ENDERECO` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `BAIRRO` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ESCOLARIDADE` varchar(40) DEFAULT NULL,
+  `FONE1` varchar(20) DEFAULT NULL,
+  `FONE2` varchar(20) DEFAULT NULL,
+  `FONE3` varchar(20) DEFAULT NULL,
+  `EMAIL` varchar(150) DEFAULT NULL,
+  `ID_CURSO` int NOT NULL DEFAULT '0',
+  `DATANASC` date DEFAULT NULL,
+  `UF` char(2) NOT NULL,
+  `CPF` char(11) NOT NULL,
+  `IDENTIDADE` varchar(20) NOT NULL,
+  `CIDADE` varchar(100) NOT NULL,
+  `OBSERVACAO` blob,
+  `BANCO` char(5) DEFAULT NULL,
+  `AGENCIA` char(6) DEFAULT NULL,
+  `CONTA` char(15) DEFAULT NULL,
+  `TITULAR` varchar(90) DEFAULT NULL,
+  `SEXO` char(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela casaazul.instrutores: ~0 rows (aproximadamente)
 
 -- Copiando estrutura para tabela casaazul.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -54,12 +95,14 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nome` varchar(180) DEFAULT NULL,
   `login` varchar(50) DEFAULT NULL,
   `senha` varchar(30) DEFAULT NULL,
-  `tipo` char(1) DEFAULT NULL,
+  `tipo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `ativo` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Copiando dados para a tabela casaazul.usuarios: ~0 rows (aproximadamente)
+INSERT INTO `usuarios` (`id`, `nome`, `login`, `senha`, `tipo`, `ativo`) VALUES
+	(1, 'Glaison Queiroz', 'Glaison', 'U2FiYXJhQDIwMjY=', 'Administrador', 'S');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
